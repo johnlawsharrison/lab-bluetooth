c6f4f54f5f3d00f4e483f142c6b0f89952ab93f2
John Laws Harrison <johnlawsharrison@gmail.com>
2017-06-04 13:40:18 -0700

    Submit lab work

--- Application/src/main/java/com/example/android/bluetoothchat/DeviceListActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/DeviceListActivity.java
@@ -103,6 +103,10 @@ public class DeviceListActivity extends Activity {
         /**
          * TODO: Register the BroadcastReceiver (mReceiver) to respond to intents with the ACTION_FOUND and ACTION_DISCOVERY_FINISHED actions
          */
+            IntentFilter intentFilterFound = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, intentFilterFound);
+            IntentFilter intentFilterFinished = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+            registerReceiver(mReceiver, intentFilterFinished);
 
 
         // Get the local Bluetooth adapter
@@ -153,7 +157,10 @@ public class DeviceListActivity extends Activity {
          * TODO: If the adapter is discoverying, cancel that discovery
          * TODO: Either way, then tell the adapter to startDiscovery
          */
-
+        if (mBtAdapter.isDiscovering()) {
+            mBtAdapter.cancelDiscovery();
+        }
+        mBtAdapter.startDiscovery();
     }
 
     /**
